5ed7a13ccbdc15642bd3458fe138eb99030f3a2b
takato <ltakato@hotmail.com>
2023-11-23 22:45:47 -0300

    struct comparison

--- main.go
+++ main.go
@@ -16,16 +16,42 @@ func main() {
 		numeroConta:   123456,
 		saldo:         125.5,
 	}
-	contaDaBruna := ContaCorrente{
-		"Bruna", 222, 111222, 200,
+
+	// fmt.Println(contaDoTakatittos)
+
+	contaDoTakatittos2 := ContaCorrente{
+		titular:       "Takatittos",
+		numeroAgencia: 589,
+		numeroConta:   123456,
+		saldo:         125.5,
 	}
 
-	fmt.Println(contaDoTakatittos)
-	fmt.Println(contaDaBruna)
+	// go knows how to compare structs! (will not compare addresses for example)
+	// will compare the content itself
+	fmt.Println(contaDoTakatittos == contaDoTakatittos2)
+
+	// contaDaBruna := ContaCorrente{
+	// 	"Bruna", 222, 111222, 200,
+	// }
+
+	// fmt.Println(contaDaBruna)
 
 	var contaDaCris *ContaCorrente
 	contaDaCris = new(ContaCorrente)
 	contaDaCris.titular = "Cris"
 
+	var contaDaCris2 *ContaCorrente
+	contaDaCris2 = new(ContaCorrente)
+	contaDaCris2.titular = "Cris"
+
+	// will compare address!!
 	fmt.Println(contaDaCris)
+	fmt.Println(contaDaCris2)
+	fmt.Println(contaDaCris == contaDaCris2)
+
+	// will compare content
+	fmt.Println(*contaDaCris)
+	fmt.Println(*contaDaCris2)
+	fmt.Println(*contaDaCris == *contaDaCris2)
+
 }
